6cc35cb05b99f8f8c90e59b016a9205b9401f46e
MostafaMohamed <sasafada46@gmail.com>
2025-12-19 01:46:33 +0200

    Fix: split init.sql and adjusted schema

---
+++ src/Database/02_triggers.sql
@@ -0,0 +1,42 @@
+CREATE OR REPLACE FUNCTION prevent_negative_quantity()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.Quantity < 0 THEN
+        RAISE EXCEPTION 'Book quantity cannot be negative';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_prevent_negative_quantity
+BEFORE UPDATE OF Quantity
+ON Book
+FOR EACH ROW
+EXECUTE FUNCTION prevent_negative_quantity();
+
+CREATE OR REPLACE FUNCTION auto_order_on_threshold()
+RETURNS TRIGGER AS $$
+DECLARE
+    order_quantity INT := 50;
+BEGIN
+    IF NEW.Quantity < NEW.Threshold AND OLD.Quantity >= OLD.Threshold THEN
+        INSERT INTO "Order"(OrderDate,"Status",TotalPrice,PubID,CustName)
+        VALUES (CURRENT_DATE,'Pending',order_quantity * NEW.SellingPrice,NEW.PubID, current_setting('postgres.current_user'));
+
+        INSERT INTO OrderItem(ISBN,OrderNum,Quantity,UnitPrice)
+        VALUES (
+            NEW.ISBN,
+            currval(pg_get_serial_sequence('"Order"','OrderID')),
+            order_quantity,
+            NEW.SellingPrice
+        );
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_auto_order_on_threshold
+AFTER UPDATE OF Quantity
+ON Book
+FOR EACH ROW
+EXECUTE FUNCTION auto_order_on_threshold();
