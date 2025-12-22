32814b4ce77e4d56d915dc26d356a57b538eea24
Mohamed Ahmed Shal <mohamed.ahmed.shal@gmail.com>
2025-12-22 14:35:27 +0200

    Made address nullable

--- src/Backend/OrderProcessing.Domain/Entities/User.cs
+++ src/Backend/OrderProcessing.Domain/Entities/User.cs
@@ -9,7 +9,7 @@ namespace OrderProcessing.Domain.Entities
         public string PhoneNumber { get; private set; }
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
-        public string Address { get; private set; }
+        public string? Address { get; private set; }
         public string PasswordHash { get; private set; }
         public UserTypes Role { get; private set; } // Roles defined in ValueObjects
         public User(
